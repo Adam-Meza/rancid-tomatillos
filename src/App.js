c7e66028e6bd0e911aa311605d4f333d1d045aac
Adam Meza <Adammeza95@gmail.com>
2023-05-11 16:53:33 -0400

    basic component layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-
+import './Components/Movie/Movie.js'
+import './Components/Header/Header.js'
+import './Components/MovieDetails/MovieDetails.js'
+import './Components/MovieContainer/MovieContainer.js'
 import './App.css';
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
+    this.state = {
+      movies: [],
+      currentMovie: {},
+      error: ''
+    }
   }
+
+  componentDidMount = () => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(data => data.json())
+      .then(json => this.setState({ movies: json.movies }))
+      .catch(err => this.setState({ error: err.message}))
+  }
+
   render() {
     return (
-      <></>
+      <main> 
+        {/* <Header /> */}
+        {/* add conditional rendering here <MovieContainer/ >
+        add conditional rendering here <MovieDetails/ > */}
+      </main>
     )
   }
 
