f21793b11a1f8dca637404badaa633bcb017db5f
Tiala Young <tialayoung@gmail.com>
2023-05-11 20:18:00 -0600

    Add styling for movie grid cards

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './Movie.css';
+import './movie.css';
 
 function Movie({ id, title, posterPath, releaseDate, voteAverage }) {
   return (
     <article className="movie-card" id={id}>
-      <img src={`${posterPath}`} alt={title} />
+      <div className='image-cont'>
+        <img src={`${posterPath}`} alt={title} />
+      </div>
       <h2>{title}</h2>
       <p>Release Date: {releaseDate}</p>
       <p>Rating: {voteAverage}</p>
