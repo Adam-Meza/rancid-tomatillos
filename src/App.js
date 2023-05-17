0fe5d91016f6cfd72ec9aa03f056f67e44ec8b03
Tiala Young <tialayoung@gmail.com>
2023-05-17 17:32:04 -0600

    Add route for NotFound

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './Components/Movie/Movie.js'
-import Header from './Components/Header/Header.js'
-import MovieDetails from './Components/MovieDetails/MovieDetails.js'
-import './Components/MovieContainer/MovieContainer.js'
-import ErrorMessage from './Components/ErrorMessage/ErrorMessage.js'
+import './Components/Movie/Movie.js';
+import Header from './Components/Header/Header.js';
+import MovieDetails from './Components/MovieDetails/MovieDetails.js';
+import './Components/MovieContainer/MovieContainer.js';
+import ErrorMessage from './Components/ErrorMessage/ErrorMessage.js';
+import NotFound from './Components/NotFound/NotFound.js';
 import './App.css';
 import React, { Component } from 'react';
 import MovieContainer from './Components/MovieContainer/MovieContainer.js';
 import { cleanAllMoviesData}  from './utilities.js';
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min.js'
-
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min.js';
 
 class App extends Component {
   constructor() {
@@ -47,6 +47,7 @@ class App extends Component {
           {/* <Route exact path='/error' render={() => <ErrorMessage error={this.state.error} />} /> */}
           <Route exact path='/movies/:id' component={MovieDetails} />
           <Route exact path='/' render={() => <MovieContainer movies={this.state.movies} />} />
+          <Route path="*" component={NotFound} />
         </Switch>
       </main>
     )
