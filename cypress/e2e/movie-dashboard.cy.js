1d1f435a1ed3b34af9cff11c826087a713091ea0
Tiala Young <tialayoung@gmail.com>
2023-05-17 16:37:51 -0600

    Remove duplicate tests, and add assertions for 'last' card displayed

--- cypress/e2e/movie-dashboard.cy.js
+++ cypress/e2e/movie-dashboard.cy.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import movies from './movies-test-data.js'
-<reference types="cypress" />
+{/* <reference types="cypress" /> */}
 
 describe('Tests for the main view displaying a library of movies', () => {
   beforeEach(() => {
@@ -23,52 +23,13 @@ describe('Tests for the main view displaying a library of movies', () => {
   });
 
   it('Should display movie cards with the movie title, image, release date, and average rating', () => {
-    cy.get('.movie-card').first().contains('h2', 'Black Adam');
-    cy.get('.movie-card').first().find('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg');
-    cy.get('.rating-text').first().contains('p', '4 / 10');
-    cy.get('.movie-card').first().contains('p', '2022');
-  });
-});
-
-describe('Tests for navigating to an individual movie details', () => {
-  beforeEach(() => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-      statusCode: 200,
-      body: {
-        movies: movies
-      }
-    });
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
-      statusCode: 200,
-      body: {
-        movie: {
-          id: 829799,
-          title: "Paradise City",
-          poster_path: "https://image.tmdb.org/t/p/original//xdmmd437QdjcCls8yCQxrH5YYM4.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//au4HUSWDRadIcl9CqySlw1kJMfo.jpg",
-          release_date: "2022-11-11",
-          overview: "Renegade bounty hunter Ryan Swan must carve his way through the Hawaiian crime world to wreak vengeance on the kingpin who murdered his father.",
-          genres: [
-          "Crime",
-          "Action",
-          "Thriller"
-          ],
-          budget: 20000000,
-          revenue: 0,
-          runtime: 93,
-          tagline: "",
-          average_rating: 1
-          }
-    }
-  });
-    cy.visit('http://localhost:3000');
-  });
-
-  it('Should redirect the user to an individual movie detail page upon click', () => {
-    cy.get('#829799').first().click();
-    // cy.url().should('include', '/829799');
-    cy.get('.background').should('have.attr', 'style', 'background-image: url("https://image.tmdb.org/t/p/original//au4HUSWDRadIcl9CqySlw1kJMfo.jpg");');
-    cy.get('h2').should('have.text', 'Paradise City');
-    cy.get('p').should('have.text', 'Renegade bounty hunter Ryan Swan must carve his way through the Hawaiian crime world to wreak vengeance on the kingpin who murdered his father.');
+    cy.get('.movie-card').first().contains('h2', 'Black Adam')
+      .get('.movie-card').last().contains('h2', 'Paradise City');
+    cy.get('.movie-card').first().find('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg')
+      .get('.movie-card').last().find('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//xdmmd437QdjcCls8yCQxrH5YYM4.jpg');
+    cy.get('.rating-text').first().contains('p', '4 / 10')
+      .get('.rating-text').last().contains('p', '1 / 10');
+    cy.get('.movie-card').first().contains('p', '2022')
+      .get('.movie-card').last().contains('p', '2022');
   });
 });
